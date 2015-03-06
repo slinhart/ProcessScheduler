830ec21750e1d7108b86113f85e80508a1d6097f
unknown <slinhart@knights.ucf.edu>
2015-03-05 21:13:55 -0500

    Initial commit for Process Scheduler program. [C# console application]

---
+++ ProcessScheduler/Parser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Parser
+{
+    string[] lines;
+
+
+	public Parser(string inFile)
+	{
+        try
+        {
+            lines = System.IO.File.ReadAllLines(@inFile);
+            Console.Write(lines[0]);
+        }
+        catch { Console.Write("Input file not found. Place input file in same directory as the executable."); }
+	}
+
+
+}
---
+++ ProcessScheduler/ProcessSchedulerControler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessScheduler
+{
+    class ProcessSchedulerController
+    {
+        static string inFile = "processes.in";
+        static string outFile = "processes.out";
+        ProcessSetModel psm;
+
+        static void Main(string[] args)
+        {
+            Parser parser = new Parser(inFile);
+            psm = parser.parse();
+            Console.Read();
+        }
+    }
+}
